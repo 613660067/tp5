9a09a533689944d49def9e4e6f53e1aba940f20d
Edik <613660067@qq.com>
2016-11-09 17:38:24 +0800

    one

--- application/index/controller/Index.php
+++ application/index/controller/Index.php
@@ -25,13 +25,44 @@ class Index extends Controller
 
         //echo $result;
 
-        $result = URL::curlGetToString("http://www.69shu.com/txt/11859.htm");
+        //$result = URL::curlGetToString("https://www.badiu.com");
 
-        $s = substr($result, 0, 100);
+        //$s = substr($result, 0, 100);
 
-        echo '111';
-        echo $s;
+
+        //echo '111';
+        //echo $result;
+        //echo $s;
 
         //return $this->fetch();
+
+        $curlobj = curl_init();			// 初始化
+        curl_setopt($curlobj, CURLOPT_URL, "http://www.69shu.com/txt/11859.htm");		// 设置访问网页的URL
+        curl_setopt($curlobj, CURLOPT_RETURNTRANSFER, true);			// 执行之后不直接打印出来
+        curl_setopt($curlobj, CURLOPT_HEADER,0);
+        $output=curl_exec($curlobj);	// 执行
+        curl_close($curlobj);			// 关闭cURL
+        //echo str_replace("百度","屌丝",$output);
+
+        $html= new \simple_html_dom();
+
+        $html->load($output);
+
+        //$main = $html->find('.status',0);
+
+        //echo $main;
+
+        $head = $html->find('meat',15);
+
+        echo $head;
+
+//        $s = substr($head, 0, 100);
+//
+//        //<meta property="og:novel:book_name" content="凤临天下：一后千宠">
+//        preg_match('/og:novel:book_name" content="(.*.)">/', $head, $heads);
+//
+//        //echo $s;
+//        dump($heads);
+
     }
 }
